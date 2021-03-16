1b271eea5890a1a0d091e5c2705b7405916bad27
gerasaavedra <69943056+gerasaavedra@users.noreply.github.com>
2021-03-16 18:32:31 -0300

    Update delilahresto.sql

--- delilahresto.sql
+++ delilahresto.sql
@@ -33,6 +33,21 @@ CREATE TABLE `orders` (
   FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
+CREATE TABLE `orders_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `product_price` decimal(10,2) DEFAULT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
+  `date_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `id` (`id`),
+  KEY `product_id` (`product_id`),
+  KEY `order_id` (`order_id`),
+  CONSTRAINT `orders_details_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `orders_details_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
+);
+
+
 
 INSERT INTO products (product_name, price, available) VALUES ('Ensalada Rusa', '600', 1);
 INSERT INTO products (product_name, price, available) VALUES ('Hamburguesa Clasica', '350', 1);
